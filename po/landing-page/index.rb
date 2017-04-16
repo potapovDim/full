f0c4e0f2a91b757d28211c832b09ba74af3bcc1e
potapovDim <dmytrop@weblium.com>
2017-04-16 14:12:59 +0300

    add modules

--- po/landing-page/index.rb
+++ po/landing-page/index.rb
@@ -1,13 +1,19 @@
+require_relative './login'
+
 class LandingPage
+  include Login
   #inititalize class variables
-  def initialize(driver)
+  def initialize(driver, username, password)
+    #user data 
+    @username                 = username
+    @password                 = password
     #webdriver instance
     @browser                  = driver
     #main selectors
     @user_login               = 'a[title="Log in"]'
     @user_signup              = 'a[title="Sign up"]'
     @get_started              = 'a[title="Get started"]'
-    @signup_and_freetrial     = 'a[title="Sign up and start trial now"]'
-    
+    @signup_and_freetrial     = 'a[title="Sign up and start trial now"]' 
   end
+
 end
--- po/landing-page/sign-up.rb
+++ po/landing-page/sign-up.rb
@@ -1,15 +1,18 @@
-module Login
+module SignUp
   @email_input                  = 'input[name="email"]'
+  @name_input                   = 'input[name="name"]'
   @password_input               = 'input[name="password"]'
-  @login_button                 = 'button[title="Login"]'
-
+  @signup_button                = 'button[type="submit"]'
+  @accept_terms                 = '#terms'
+  @captcha                      = '.recaptcha-checkbox-checkmark'  
   def self.initialize(driver)
     @browser = driver
     self
   end
-  def self.login_user(username, password)
+  def self.signup_user(username, password)
     @browser.element(css: @email_input).send_keys username
     @browser.element(css: @password_input).send_keys password
     @browser.element(css: login_button).click
   end
+  
 end
