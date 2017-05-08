79f3faf04ad735b46384a3b1b11f232b1704eb3a
potapovDim <dmytrop@weblium.com>
2017-05-08 14:38:05 +0300

    add negative tests for user login

--- spec/smokes/login_user.smoke.rb
+++ spec/smokes/login_user.smoke.rb
@@ -1,5 +1,6 @@
 require_relative '../../scripts/local.rb'
 
+# , :focus => true if need run only this suit
 describe "Login user" do
   before :each do
     @browser.window.resize_to 1200, 1600
@@ -10,20 +11,69 @@ describe "Login user" do
     el = @landing.login().fail_login_user
     expect(el.present?).to eql(true)
   end
-  it "get statrted (failed login)" do
+  it "get statrted (negative)" do
     el = @landing.get_started_login().fail_login_user
     expect(el.present?).to eql(true)
   end
-  it "sign up and start trial (failed login)" do
+  it "sign up and start trial (negative)" do
     el = @landing.signup_and_freetrial_login().fail_login_user
     expect(el.present?).to eql(true)
   end
-  it "go concepts and succes login by fourth growth plan" do
+  it "go concepts and succes login by fourth growth plan (negative)" do
     el = @landing
           .header_concepts_page()
           .go_to_login()
           .fail_login_user
     expect(el.present?).to eql(true)
+  end
+  it "go pricing and login by first growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(0)
+        .login_user(" "+ @username, @password)
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
+  end
+  it "go pricing and login by second growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(1)
+        .login_user(" "+ @username, @password)
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
+  end
+  it "go pricing and login by second growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(1)
+        .login_user(@username," "+@password)
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
+  end
+  it "go pricing and login by second growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(1)
+        .login_user(@username,@password + " ")
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
+  end
+  it "go pricing and login by second growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(1)
+        .login_user(@username, " ")
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
+  end
+  it "go pricing and login by second growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(1)
+        .login_user(@username, "------------------------------")
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
+  end
+  it "go pricing and login by second growth plan (negative)" do
+    @landing
+        .header_pricing_page()
+        .success_login_by_growth_plan(1)
+        .login_user(@username, "@#&*&Y$#*&^*$#")
+    expect(@browser.url.include?('/myaccount/account/websites')).to eql(false)
   end
   #success part login
   it "sucess login user" do
