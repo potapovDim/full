f0a9e769c0e52f7a2b36d2566ddf927492d2137a
potapovDim <dmytrop@weblium.com>
2017-05-21 13:55:58 +0300

    add negative tests

--- js/e2e/tests/login.test.js
+++ js/e2e/tests/login.test.js
@@ -18,21 +18,59 @@ module.exports = {
     client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
     client.end()
   },
-  'failed login user (edge conditin name)': client => {
+  'failed login user (edge conditin name bafore )': client => {
     const loginPage = client.page.login()
     client.resizeWindow(1200, 900)
     loginPage.navigate()
              .waitApp()
-             .loginUserSuccess(' '+USER_NAME, USER_PASSWORD)
+             .loginUserFailed(' '+USER_NAME, USER_PASSWORD)
     client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
     client.end()
   },
-  'failed login user (edge conditin password)': client => {
+  'failed login user (edge conditin name after )': client => {
     const loginPage = client.page.login()
     client.resizeWindow(1200, 900)
     loginPage.navigate()
              .waitApp()
-             .loginUserSuccess(USER_NAME,' ' + USER_PASSWORD)
+             .loginUserFailed(USER_NAME+ ' ', USER_PASSWORD)
+    client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
+    client.end()
+  },
+  'failed login user (edge conditin password before)': client => {
+    const loginPage = client.page.login()
+    client.resizeWindow(1200, 900)
+    loginPage.navigate()
+             .waitApp()
+             .loginUserFailed(USER_NAME,' ' + USER_PASSWORD)
+    client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
+    client.end()
+  },
+  'failed login user (edge conditin password after)': client => {
+    const loginPage = client.page.login()
+    client.resizeWindow(1200, 900)
+    loginPage.navigate()
+             .waitApp()
+             .loginUserFailed(USER_NAME, USER_PASSWORD + ' ')
+    client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
+    client.end()
+  },
+  'failed login user (regex password)': client => {
+    const loginPage = client.page.login()
+    client.resizeWindow(1200, 900)
+    loginPage.navigate()
+             .waitApp()
+             .loginUserFailed(USER_NAME, 	
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
+    client.end()
+  },
+  'failed login user (regex email)': client => {
+    const loginPage = client.page.login()
+    client.resizeWindow(1200, 900)
+    loginPage.navigate()
+             .waitApp()
+             .loginUserFailed(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                USER_PASSWORD)
     client.assert.urlEquals(`${LINK_TO_RESOURCE}login`)
     client.end()
   }
