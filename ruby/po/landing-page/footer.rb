08af27c49bfd0ea30211ac6d11057943f68fa8be
Bohdan <bohdan@Bohdans-Mac-mini.local>
2017-09-07 14:26:04 +0300

    add footer link check

--- ruby/po/landing-page/footer.rb
+++ ruby/po/landing-page/footer.rb
@@ -12,20 +12,53 @@ class Footer
     #
     @blog                     = 'a[href="http://blog.weblium.com/"]'
     @facebook                 = 'a[href="https://www.facebook.com/weblium/"]'
-    @twitter                  = 'a[href="https://twitter.com/WebliumTeam"]'
+    @twitter                  = 'a[href="https://twitter.com/Weblium"]'
     @linkedin                 = 'a[href="https://www.linkedin.com/company-beta/17969770/"]'
     #
+    @support_center           = 'a[href="http://support.weblium.com/"]'
     @privacy_policy           = 'a[href="/policy"]'
-    @terms_of_use             = 'a[href="/terms"]'
+    @terms_of_use             = 'a[href="/terms"]'             
   end
-
-  def pricing_page
+  def get_pricing_page
     @browser.element(css: @pricing).click
     return Pricing.new @browser
   end
 
-  def concepts_page
+  def get_concepts_page
     @browser.element(css: @concepts).click
     return Concepts.new @browser
   end
+  def go_to_pricing_page
+    @browser.element(css: @pricing).click
+  end
+
+  def go_to_concepts_page
+    @browser.element(css: @concepts).click
+  end
+
+  def go_to_about
+    @browser.element(css: @about).click 
+  end
+  def go_to_blog
+    @browser.element(css: @blog).click 
+  end
+  def go_to_facebook
+    @browser.element(css: @facebook).click 
+  end
+  def go_to_twitter
+    @browser.element(css: @twitter).click 
+  end
+  def go_to_linkedin
+    @browser.element(css: @linkedin).click 
+  end
+  def go_to_support_center
+    @browser.element(css: @support_center).click 
+  end
+  def go_to_privacy_policy
+    @browser.element(css: @privacy_policy).click 
+  end
+  def go_to_terms_of_use
+    @browser.element(css: @terms_of_use).click 
+  end
+
 end
--- ruby/spec/e2e/navigation_landing.e2e.rb
+++ ruby/spec/e2e/navigation_landing.e2e.rb
@@ -32,4 +32,50 @@ describe 'Navigation' do
     @landing.go_to_landing()
     expect(@browser.url).to eq(@base_url)
   end
+  it 'assert footer link exists' do
+    @footer = Footer.new(@browser)
+    @footer.go_to_pricing_page
+    expect(@browser.url.include? '/pricing').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_concepts_page
+    expect(@browser.url.include? '/concepts').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_about
+    expect(@browser.url.include? '/about').to eql(true)
+    @browser.goto @base_url
+
+    @footer.go_to_blog
+    @browser.driver.switch_to.window(@browser.driver.window_handles[1])
+    expect(@browser.url.include? 'http://blog.weblium.com/').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_facebook
+    @browser.driver.switch_to.window(@browser.driver.window_handles[2])
+    expect(@browser.url.include? 'www.facebook.com/weblium/').to eql(true)
+    @browser.goto @base_url 
+    
+    @footer.go_to_twitter
+    @browser.driver.switch_to.window(@browser.driver.window_handles[3])
+    expect(@browser.url.include? 'twitter.com/Weblium').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_linkedin
+    @browser.driver.switch_to.window(@browser.driver.window_handles[4])    
+    expect(@browser.url.include? 'www.linkedin.com').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_support_center
+    expect(@browser.url.include? 'https://weblium.zendesk.com').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_privacy_policy
+    expect(@browser.url.include? '/policy').to eql(true)
+    @browser.goto @base_url 
+
+    @footer.go_to_terms_of_use
+    expect(@browser.url.include? '/terms').to eql(true)
+    @browser.goto @base_url 
+  end
 end
