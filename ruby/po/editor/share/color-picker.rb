8185dea81f0ee4e8e3bf9935d8f02d4858bf9a44
Bohdan <bohdan@Bohdans-Mac-mini.local>
2017-09-26 17:17:21 +0300

    row background test

--- ruby/po/editor/share/color-picker.rb
+++ ruby/po/editor/share/color-picker.rb
@@ -2,27 +2,41 @@ class ColorPicker
   def initialize(browser)
     @browser = browser
     @opacity_picker_pointer = '.pointer_35hVk'
-    @color_input = 'input[type="text"]'
-    @palette_item = '[data-test="color-item-palette"]'
-    @alpha_input = '[data-test="input-number-input"]'
+    @color_input            = 'input[type="text"]'
+    @palette_item           = '[data-test="color-item-palette"]'
+    @alpha_input            = '[data-test="input-number-input"]'
+    @reset_button           = 'button[title="Reset"]'
+    #input elements
+    @color_element          = @browser.element(css: @color_input)
+    @opacity_element        = @browser.element(css: @alpha_input)
   end
-  def input_color_palette(color)
-    @browser.element(css: @color_input).send_keys color
+  attr_reader :color_element, :opacity_element
+  
+  # Do lytchih vremen
+  # def self.my_attr_reader(var_name)
+  #   define_method(var_name) do
+  #     return @browser.element(css: instance_variable_get(("@#{var_name}")))
+  #   end
+  # end
+  # my_attr_reader :color_input
+  # my_attr_reader :alpha_input
+
+  def drag_opacity_picker(x,y)
+    dnd_element(@opacity_picker_pointer,x,y)
   end
   def clear_color_input()
     clear_input(@color_input)
     return self
   end
-  def input_alpha_palette(percent)
-    @browser.element(css: @alpha_input).send_keys percent
-  end
-
-  def change_alpha_slider_palette(x)
-    @browser.element(css: @color_picker_pointer).fire_event "mouseover"  
-    @browser.element(css: @color_picker_pointer).drag_and_drop_by x, 0
+  def input_alpha_palette()
+    clear_input(@alpha_input)
+    return self
   end
 
   def click_palette_color(index)
     @browser.elements(css: @palette_item)[index].fire_event 'click'
+  end
+  def reset
+    @browser.element(css: @reset_button).click
   end
 end
--- ruby/spec/e2e/structure.e2e.rb
+++ ruby/spec/e2e/structure.e2e.rb
@@ -16,59 +16,82 @@ describe 'Row' do
 
   it 'row background (color)' do
     initial_background = @row.get_row_background
-    puts initial_background
-    @row_settings.choose_tab("background")
-        .swich_tab("Color")
-        .clear_color_input()
-        .input_color_palette("#AAAAAA")
+    color_tab = @row_settings.choose_tab("background")
+                .swich_tab("Color")
+                
+    color_tab.clear_color_input()
+             .color_element.send_keys("#AAAAAA")
+#check color input
     expect(@row.get_row_background).to_not eql(initial_background)
     expect(@row.get_row_background.include?("rgb(170, 170, 170)")).to eql(true)
+#set opacity using dnd to 65
+    color_tab.drag_opacity_picker(-30,0)
+    sleep 0.2
+    expect(@row.get_row_background.include?("rgba(170, 170, 170, 0.65)")).to eql(true)
+    expect(color_tab.opacity_element.value).to eql("65")
+#set opacity using dnd to 100
+    color_tab.drag_opacity_picker(31,0)
+    sleep 0.2
+    expect(@row.get_row_background.include?("rgb(170, 170, 170)")).to eql(true)
+    expect(color_tab.opacity_element.value).to eql("100")
+#alpha input check
+    color_tab.input_alpha_palette
+             .opacity_element.send_keys(15)
+    sleep 0.2    
+    expect(@row.get_row_background.include?("rgba(170, 170, 170, 0.15)")).to eql(true)
+#set color by color picker 
+#check is opacity saved
+    color_tab.click_palette_color(15)
+    sleep 0.2            
+    expect(@row.get_row_background.include?('rgba(255, 202, 40, 0.15)')).to eql(true)
+    expect(color_tab.color_element.value).to eql("#FFCA28")
+#check reset function
+    color_tab.reset
+    sleep 0.2   
+    expect(@row.get_row_background).to eql(initial_background)    
+    expect(color_tab.color_element.value).to eql("#FFFFFF")
+    expect(color_tab.opacity_element.value).to eql("100")
+    end
+#   it 'row background (gradient)' do
+#     initial_background = @row.get_row_background
+#     @row.click_row_context('settings')
+#         .choose_tab("background")
+#         .change_background_type("gradient")
+#     @browser.send_keys :escape
+#     changed_background = @row.get_row_background
+#     expect(changed_background).to_not eql(initial_background)
+#   end
+  it 'row layout' do
+    layout_tab = LayoutTab.new(@browser)
+    layout_tab.change_layout(4)
+    changed_row_layout = @row.get_row_layout 
+#switch to 4 layout
+    expect(changed_row_layout).to eql(4)
+    expect(layout_tab.custom_width_presented?).to eql(true)
+#switch to first layout
+    layout_tab.change_layout(1)
+    expect(@row.get_row_layout).to eql(1)
+#switch to 5th layout
+    layout_tab.change_layout(5)
+    expect(layout_tab.layout_active?(5)).to eql(true)
+    expect(@row.get_row_layout).to eql(6)
+#drag last sosochek to the right
+    expect(layout_tab.get_layout_postion(5)).to eql("10")
+    layout_tab.create_custom_layout(20,0,5)    
+    expect(layout_tab.get_layout_postion(5)).to eql("11")
+    expect(layout_tab.layout_active?(5)).to eql(false)
+#drag last sosochek to the left
+    layout_tab.create_custom_layout(-20,0,5)
+    expect(layout_tab.get_layout_postion(5)).to eql("10")
+    expect(layout_tab.layout_active?(5)).to eql(true)
+#check no-intend-functionality
+    margin_before = @row.intend_between_columns?
+    layout_tab.nomargin_click
+    expect(@row.intend_between_columns?).to_not eql(margin_before)
+    layout_tab.nomargin_click
+    expect(@row.intend_between_columns?).to eql(margin_before)  
   end
 end
-#     # @browser.send_keys :escape
-#     # changed_background = @row.get_row_background
-#     # expect(changed_background).to_not eql(initial_background)
-#   end
-  # it 'row background (gradient)' do
-  #   initial_background = @row.get_row_background
-  #   @row.click_row_context('settings')
-  #       .choose_tab("background")
-  #       .change_background_type("gradient")
-  #   @browser.send_keys :escape
-  #   changed_background = @row.get_row_background
-  #   expect(changed_background).to_not eql(initial_background)
-  # end
-#   it 'row layout' do
-#     layout_tab = LayoutTab.new(@browser)
-#     layout_tab.change_layout(4)
-#     changed_row_layout = @row.get_row_layout 
-# #switch to 4 layout
-#     expect(changed_row_layout).to eql(4)
-#     expect(layout_tab.custom_width_presented?).to eql(true)
-# #switch to first layout
-#     layout_tab.change_layout(1)
-#     expect(@row.get_row_layout).to eql(1)
-# #switch to 5th layout
-#     layout_tab.change_layout(5)
-#     expect(layout_tab.layout_active?(5)).to eql(true)
-#     expect(@row.get_row_layout).to eql(6)
-# #drag last sosochek to the right
-#     expect(layout_tab.get_layout_postion(5)).to eql("10")
-#     layout_tab.create_custom_layout(20,0,5)    
-#     expect(layout_tab.get_layout_postion(5)).to eql("11")
-#     expect(layout_tab.layout_active?(5)).to eql(false)
-# #drag last sosochek to the left
-#     layout_tab.create_custom_layout(-20,0,5)
-#     expect(layout_tab.get_layout_postion(5)).to eql("10")
-#     expect(layout_tab.layout_active?(5)).to eql(true)
-# #check no-intend-functionality
-#     margin_before = @row.intend_between_columns?
-#     layout_tab.nomargin_click
-#     expect(@row.intend_between_columns?).to_not eql(margin_before)
-#     layout_tab.nomargin_click
-#     expect(@row.intend_between_columns?).to eql(margin_before)  
-#   end
-# end
 # , :focus => true if need run only this suit
 # describe 'Block component' do
 #   before :each do
