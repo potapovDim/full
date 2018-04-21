a50770f53e5a81bffdb1bfc690fb2c6306067bfa
Potapov Dmitriy <potapovd3012@gmail.com>
2018-04-21 15:19:49 +0300

    fix

--- backoffice/app/models/blocks/block.ts
+++ backoffice/app/models/blocks/block.ts
@@ -42,9 +42,32 @@ class Preview {
 class Categories {
 
   private root: Element
+  private pickCategoryOpen: Element
+  private options: Elements
+  private categories: Elements
 
   constructor() {
     this.root = $$('.card').get(1)
+    this.pickCategoryOpen = this.root.$('.mat-input-infix.mat-form-field-infix')
+    this.options = this.root.$$('mat-option[role="option"]')
+    this.categories = this.root.$$('span.tags')
+  }
+
+  public async removeCategory(categories: string | string[]) {
+    categories = Array.isArray(categories) ? categories : [categories]
+    for (const category of categories) {
+      await this.categories.forEach(async (el) => {
+        const text = await el.getText()
+        if (text === category) await el.$('mat-icon').click()
+      })
+    }
+  }
+
+  public async pickCategory(categoryName: string) {
+    const category = (await this.options.filter(async () => {
+      async (el) => await el.getText() === categoryName
+    }))[0]
+    await category.click()
   }
 }
 
